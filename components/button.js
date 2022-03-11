fd4ecf887501c53959b17d5acf342ae5e146eb9c
Lisa Lu <lisalue@posteo.de>
2022-03-11 11:18:14 +0100

    make fancy button

--- components/button.js
+++ components/button.js
@@ -1,3 +1,19 @@
 import styled from "styled-components";
-import { useState } from "react";
 
+export function Button({description}) {
+    return (
+        <FancyButton>{description}</FancyButton>
+    )
+}
+
+const FancyButton = styled.button`
+height: 5rem;
+width:auto;
+padding:1rem;
+background: rgb(165,15,216);
+background: linear-gradient(132deg, rgba(165,15,216,1) 0%, rgba(132,33,221,0.9017545391061452) 0%, rgba(193,156,30,0.9296875) 100%);
+color:white;
+text-transform:uppercase;
+border-radius:2rem;
+
+`
